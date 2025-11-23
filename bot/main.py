721e49e19aa7a09276f636696f86913c03bf2ef1
Your Name <you@example.com>
2025-11-23 14:16:35 +0300

    base bot

---
+++ bot/main.py
@@ -0,0 +1,21 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from decouple import config
+
+
+bot = Bot(token = config('TOKEN')) # type: ignore
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
